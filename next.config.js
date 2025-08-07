0bff3523405d07717de870e8f7dd9e38ea457cda
Vsevolod Zolotov <vsezold@gmail.com>
2025-08-07 16:44:48 +0300

    fix

--- next.config.js
+++ next.config.js
@@ -5,6 +5,13 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  webpack: (config) => {
+    config.externals.push({
+      "utf-8-validate": "commonjs utf-8-validate",
+      bufferutil: "commonjs bufferutil",
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
